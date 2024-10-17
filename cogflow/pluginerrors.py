deb3eb2a042e75fec9e6a0bed3d8f236d4e64dcc
Bola Nasr <100923304+Bola-Nasr@users.noreply.github.com>
2024-10-17 14:55:42 +0200

    Revert "1.9.39b8"

--- cogflow/pluginerrors.py
+++
@@ -1,9 +0,0 @@
-"""
-Module containing custom exception classes for plugin errors.
-"""
-
-
-class PluginErrors(Exception):
-    """
-    Custom exception class for plugin errors.
-    """
--- cogflow/v2/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-KFP v2
-"""
-
-from kfp import v2 as kfp_v2
-
-globals().update(vars(kfp_v2))
