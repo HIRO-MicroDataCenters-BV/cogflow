03a822506fa4771d51b9bd8aa655b79ca8974e41
Sai Kireeti <sai.kireeti@hiro-microdatacenters.nl>
2025-09-19 09:21:44 +0200

    1.10.11

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("LICENSE.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cogflow",
-    version="1.10.13",
+    version="1.11.0",
     author="Sai_kireeti",
     author_email="sai.kireeti@hiro-microdatacenters.nl",
     description="COG modules",
